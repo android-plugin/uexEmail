844d8f69a16d44697d1dd14be8e9d9f62f7f7be9
JingweiWang <13622032005@qq.com>
2016-02-16 10:10:54 +0800

    Add QQmail support
    1. Add QQmail support
    2. Fix project

--- src/org/zywx/wbpalmstar/plugin/uexemail/EUExEmail.java
+++ src/org/zywx/wbpalmstar/plugin/uexemail/EUExEmail.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2016.  The AppCan Open Source Project.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ */
 package org.zywx.wbpalmstar.plugin.uexemail;
 
 import java.util.ArrayList;
@@ -75,7 +93,7 @@ public class EUExEmail extends EUExBase {
 						}
 					}
 					intent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, imageUris);
-					intent.setType("message/rfc882");
+					intent.setType("application/octet-stream");
 					intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 					startActivity(Intent.createChooser(intent, EUExUtil.getString("plugin_uexEmail_select_app_to_send_email")));
 				}
